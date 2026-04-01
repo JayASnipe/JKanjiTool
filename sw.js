8d92915ae412d16fda8d194e7e6bf75f8d38c676
JayASnipe <jay.snipe@gmail.com>
2026-04-01 15:45:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // J Kanji Service Worker v5
-const CACHE = 'kanji-202604011508';
+const CACHE = 'kanji-202604011545';
 
 const PRECACHE = [
   './',
@@ -7,12 +7,12 @@ const PRECACHE = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  './app-compiled.js?v=202604011508',
-  './data/grammar.json?v=202604011508',
-  './data/thematic.json?v=202604011508',
-  './data/spicy.json?v=202604011508',
-  './data/grade1.json?v=202604011508',
-  './data/grades2-5.json?v=202604011508',
+  './app-compiled.js?v=202604011545',
+  './data/grammar.json?v=202604011545',
+  './data/thematic.json?v=202604011545',
+  './data/spicy.json?v=202604011545',
+  './data/grade1.json?v=202604011545',
+  './data/grades2-5.json?v=202604011545',
 ];
 
 self.addEventListener('install', e => {
